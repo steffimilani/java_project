ee868e70e0da8bd142907e2ef7848778d17bda0b
steffimilani <steffimilani7798@gmail.com>
2026-07-21 15:17:44 +0530

    new

---
+++ src/program4.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 3;
+
+        // Integer Division
+        int intResult = a / b;
+        System.out.println("Integer Division: " + intResult);
+
+        // Floating-Point Division
+        double floatResult = (double) a / b;
+        System.out.println("Floating-Point Division: " + floatResult);
+    }
+}
