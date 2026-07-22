9bbdcb8d5191c69dd7ec6fd720ef2ae93b6132ee
steffimilani <steffimilani7798@gmail.com>
2026-07-22 09:40:19 +0530

    new

---
+++ src/day11/program1.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean squareIsWhite(String coordinates) {
+        int column = coordinates.charAt(0) - 'a' + 1;
+        int row = coordinates.charAt(1) - '0';
+
+        return (column + row) % 2 != 0;
+    }
+}
